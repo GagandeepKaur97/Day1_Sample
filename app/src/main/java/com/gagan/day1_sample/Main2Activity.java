ef1edb80727768bba3cce850bc7e6483f7a8fdab
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-02 16:35:52 -0400

    [gagandeepkaur]passing data

--- app/src/main/java/com/gagan/day1_sample/Main2Activity.java
+++ app/src/main/java/com/gagan/day1_sample/Main2Activity.java
@@ -1,14 +1,39 @@
 package com.gagan.day1_sample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
+    private static final String TAG = "Main2Activity";
 
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.d(TAG,"onCreate :Started.");
+        initImageBitmaps();
+    }
+    private void   initImageBitmaps(){
+        Log.d(TAG,"  initImageBitmaps:preparing bitmaps.");
+        mImageUrls.add("https://media1.picsearch.com/is?kFbHfprJq4unGL6SXd6v5bL7JK_HozKD9cIoYo14VF4&height=161");
+        mNames.add("Nigara Fall");
+        mImageUrls.add("https://media4.picsearch.com/is?E9YZaJ4lwxO1RTHLeHbs7tNzuPYjQyTW3EeEpk6Z9A4&height=255");
+        mImageUrls.add("CN tower");
+initRecyclerview();
+    }
+    private void initRecyclerview(){
+        Log.d(TAG,"initRecyclerview:init recyclerview.");
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerviewadapter adapter = new recyclerviewadapter(this,mNames,mImageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/gagan/day1_sample/recyclerviewadapter.java
+++ app/src/main/java/com/gagan/day1_sample/recyclerviewadapter.java
@@ -1,32 +1,69 @@
 package com.gagan.day1_sample;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.Constraints;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class recyclerviewadapter extends RecyclerView.Adapter<recyclerviewadapter.viewHolder> {
     private static final String TAG = "recyclerviewadapter";
+    private ArrayList<String> mImagesNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public recyclerviewadapter(Context mContext ,ArrayList<String> mImagesNames, ArrayList<String> mImages ) {
+        this.mImagesNames = mImagesNames;
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+
 
     @NonNull
     @Override
-    public recyclerviewadapter.viewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public recyclerviewadapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from( parent.getContext()).inflate(R.layout.listitems,parent,false);
+        viewHolder holder = new viewHolder(view) ;
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull recyclerviewadapter.viewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull recyclerviewadapter.viewHolder holder, final int position) {
+        Log.d(TAG,"onBindViewHolder:called.");
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.image);
+        holder.imagename.setText(mImagesNames.get(position));
+        holder.parentLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"onClick:clicked on:"+ mImagesNames.get(position));
+                Toast.makeText(mContext,mImagesNames.get(position),Toast.LENGTH_SHORT).show();
+
+            }
+
+
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mImagesNames.size();
     }
 
     public class viewHolder extends RecyclerView.ViewHolder {
